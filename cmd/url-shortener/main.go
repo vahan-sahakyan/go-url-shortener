6cd3fbf7189e8966b4942653cffddc5e56a66da9
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2024-12-28 00:12:16 +0400

    init

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "fmt"
+  "log/slog"
+  "os"
+  "url-shortener/internal/config"
+)
+
+const (
+  envLocal = "local"
+  envDev   = "dev"
+  envProd  = "prod"
+)
+
+func main() {
+  cfg := config.MustLoad()
+
+  fmt.Println(cfg)
+  log := setupLogger(cfg.Env)
+
+  log.Info("starting url-shortener", slog.String("env", cfg.Env))
+  log.Debug("debug messages are enabled")
+
+  // TODO: init storage: sqlite
+  // TODO: init router: chi, "chi render"
+  // TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+  var log *slog.Logger
+  switch env {
+  case envLocal:
+    log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+  case envDev:
+    log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+  case envProd:
+    log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+  }
+  return log
+}
