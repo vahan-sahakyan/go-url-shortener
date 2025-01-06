ce6fdcd8ca5fa1cb123299893c5df84e40e07c58
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2025-01-06 19:02:08 +0400

    cleanup

--- internal/lib/logger/handlers/slogdiscard/slogdiscard.go
+++ internal/lib/logger/handlers/slogdiscard/slogdiscard.go
@@ -1,36 +1,32 @@
 package slogdiscard
 
 import (
-	"context"
-	"log/slog"
+  "context"
+  "log/slog"
 )
 
 func NewDiscardLogger() *slog.Logger {
-	return slog.New(NewDiscardHandler())
+  return slog.New(NewDiscardHandler())
 }
 
 type DiscardHandler struct{}
 
 func NewDiscardHandler() *DiscardHandler {
-	return &DiscardHandler{}
+  return &DiscardHandler{}
 }
 
 func (h *DiscardHandler) Handle(_ context.Context, _ slog.Record) error {
-	// Просто игнорируем запись журнала
-	return nil
+  return nil
 }
 
 func (h *DiscardHandler) WithAttrs(_ []slog.Attr) slog.Handler {
-	// Возвращает тот же обработчик, так как нет атрибутов для сохранения
-	return h
+  return h
 }
 
 func (h *DiscardHandler) WithGroup(_ string) slog.Handler {
-	// Возвращает тот же обработчик, так как нет группы для сохранения
-	return h
+  return h
 }
 
 func (h *DiscardHandler) Enabled(_ context.Context, _ slog.Level) bool {
-	// Всегда возвращает false, так как запись журнала игнорируется
-	return false
+  return false
 }
